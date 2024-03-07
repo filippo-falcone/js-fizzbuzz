3b817af06db101c4057dc5abda848a073b5d324b
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-07 15:29:08 +0100

    feat: add number to html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Scrivere un programma che stampi in console i numeri da 1 a 100
+const numberContainer = document.querySelector('#number-container');
 // Creo un ciclo che va da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Assegno ai numeri una stringa quando è multiplo di 3, di 5 o di entrambi
@@ -11,5 +12,20 @@ for (let i = 1; i <= 100; i++) {
         fizzOrAndBuzz = 'Buzz';
     }
     // Stampo i numeri all'utente
-    console.log(fizzOrAndBuzz);
+    // - Per ogni numero creo un elemento da inserire nell'html
+    const number = document.createElement('div');
+    // - Inserisco il contenuto da stampare
+    number.innerHTML = fizzOrAndBuzz;
+    // - Inserisco le classi
+    number.classList.add('ms-number');
+    number.classList.add('mx-2');
+    number.classList.add('my-2');
+    number.classList.add('rounded');
+    number.classList.add('bg-light');
+    number.classList.add('text-center');
+    number.classList.add('d-flex');
+    number.classList.add('align-items-center');
+    number.classList.add('justify-content-center');
+    // - Appendo l'elemento al container
+    numberContainer.append(number);
 }
